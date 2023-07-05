13fad2a00a70f8f823f1fc9867457d7bbe6a509b
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-05 18:54:07 +0530

    added demo data and rendered dynamically on page.

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,3 +1,37 @@
+
+//Making test object 
+const todos = [
+    {
+        _id: "1",
+        description: "I have to wake up",
+        due_date: "10/20/2924",
+        category: "Sleep",
+        completed: false
+    },
+    {
+        _id: "2",
+        description: "Complete Homework",
+        due_date: "10/20/2924",
+        category: "School",
+        completed: true
+    },
+    {
+        _id: "3",
+        description: "Hair cut",
+        due_date: "10/20/2924",
+        category: "Cleaning",
+        completed: false
+    },
+    {
+        _id: "4",
+        description: "Diary update",
+        due_date: "10/20/2924",
+        category: "Habbit",
+        completed: true
+    },
+];
+
 module.exports.todo = function (req, res){
-    return res.render('todo');
+
+    return res.render('todo', {todos});
 }
