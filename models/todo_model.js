37ec7551ece188e18879638e928c0374cab81107
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-06 00:07:16 +0530

    added todo schema

---
+++ models/todo_model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    description: String,
+    due_date: Date,
+    category: String,
+    createdOn: {type: Date, default: Date.now()}
+});
+
+const todoModel = mongoose.model('todo', todoSchema);
+
+module.exports.todoModel = todoModel;
